71f60bc60954079f3a494718b245f39a9b655bec
d2mpyk <d2mpyk@gmail.com>
2024-02-25 01:13:07 -0500

    Actualizacion de .env, main.py, README.md

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-<h1>Bot Discord en Python</h1>
+# Bot Discord en Python
 
 - Un simple bot para Discord realizado en Python 
-- Indica las noticias de los mercados financieros, cuando alguien pregunta <strong>/noticias</strong> en el chat
+- Indica las noticias de los mercados financieros, cuando alguien tipea <strong>/noticias</strong> en el chat
+
+### Los pasos para crear el bot en Discord se pueden encontrar en el siguiente enlace: 
+
+- [Instructivo en Real Python](https://realpython.com/how-to-make-a-discord-bot-python/)
+
+### Los paquetes de Python a Instalar son:
+
+    pip install --upgrade pip
+    pip install requests json
+    pip install -U discord.py
+    pip install -U python-dotenv
+
+### Lo siguiente es un ejemplo del codigo que va en el archivo .env
+    
+    TOKEN = AquiVaElTokenDeDiscord,SinComillas,SinLlaves,SinCorchetes
+
+### Para ejecutar
+
+    py main.py
+
--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import requests, json, emoji, os, discord
+import requests, json, os, discord
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv("TOKEN")
+
+url = requests.get("https://nfs.faireconomy.media/ff_calendar_thisweek.json")
+data = json.loads(url.text)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-url = requests.get("https://nfs.faireconomy.media/ff_calendar_thisweek.json")
-text = url.text
-# print(type(text))
-data = json.loads(text)
-# print(type(data))
-
 @client.event
 async def on_ready():
-    print(f'Se ha logeado como: {client.user}')
+    print(f'El Bot se ha logeado como: {client.user}')
 
 @client.event
 async def on_message(message):
@@ -31,7 +27,6 @@ async def on_message(message):
         fecha = noticia["date"].split('T')[0]
         hora = noticia["date"].split('T')[1].split('-')[0]
         manana = datetime.strftime(datetime.now() + timedelta(1), "%Y-%m-%d")
-        #impacto = emoji.emojize(":fire:")
         impacto = "\N{fire}"
         if noticia["impact"] == "Medium":
             impacto *= 2
@@ -39,9 +34,15 @@ async def on_message(message):
             if noticia["impact"] == "High":
                 impacto *= 3
         if fecha == manana :
-            mensaje += noticia["country"]+", "+noticia["title"]+", Impacto: "+impacto+" "+noticia["impact"]+", fecha: "+manana+", hora: "+hora+"\n"
+            mensaje += "***"+noticia["country"]+"***, _"+noticia["title"]+"_, **Impacto**: "+impacto+" "+noticia["impact"]+", **fecha**: "+manana+", **hora**: "+hora+"\n"
 
     if message.content.startswith('/noticias'):
-        await message.channel.send(mensaje)  
+        try:
+            await message.channel.send(mensaje)
+        except:
+            await message.channel.send("No hay noticias que mostrar")
 
-client.run(TOKEN)
+        await message.channel.send('https://www.forexfactory.com/calendar')
+        
+
+client.run(os.getenv('TOKEN'))
